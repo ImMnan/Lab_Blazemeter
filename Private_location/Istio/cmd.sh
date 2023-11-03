858cb9f31a47b8cb67bced8e92e4db1fdeff5676
Manan patel <86557286+ImMnan@users.noreply.github.com>
2023-11-03 11:56:43 +0530

    update

--- Private_location/Istio/cmd.sh
+++ Private_location/Istio/cmd.sh
@@ -4,14 +4,13 @@ kubectl delete ns projectcontour
 
 ./istioctl install --set profile=demo -y OR istioctl install --set profile=demo -y
 
-kubectl label namespace <namespace> istio-injection=enabled
+kubectl label namespace bmi istio-injection=enabled
 
 openssl genrsa -out bmgo.local.key 2048
 openssl req -new -key bmgo.local.key -out bmgo.local.csr
 openssl x509 -req -days 3650 -in bmgo.local.csr -signkey bmgo.local.key -out bmgo.local.crt
 ls
 
-rm mydomain.local.key
-rm bmgo.local.csr
+rm bmgo.local.key
+##rm bmgo.local.csr
 kubectl create -n istio-system secret tls wildcard-credential --key=bmgo.local.key --cert=bmgo.local.crt 
-kubectl create -n istio-system secret tls wildcard-credential --key=bmgo.local.key --cert=bmgo.local.crt -n bm1
